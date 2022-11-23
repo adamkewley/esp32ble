6507682564081f44a8bafb77101fae0944c0b1e0
Adam Kewley <contact@adamkewley.com>
2022-11-23 09:10:09 +0100

    Updated main sketch with BLE stuff

--- sketches/meg/meg.ino
+++ sketches/meg/meg.ino
@@ -1,17 +1,30 @@
-#include <BLEDevice.h>
-#include <BLEUtils.h>
-#include <BLEServer.h>
+#include <NimBLEDevice.h>
 
 #include <cstddef>
 #include <cstdint>
 
-// macros
-#define AK_TOKENPASTE(x, y) x##y
-#define AK_TOKENPASTE2(x, y) AK_TOKENPASTE(x, y)
-#define AK_DEBUG
+// SECTION: top-level macros
+//
+// edit these to change device behavior
 
-// perf stuff
-namespace ak
+#define MEG_DEBUG
+#define MEG_BLE_LONG_NAME "MegDevice"
+#define MEG_BLE_CONFIGURATION_SERVICE_UUID "c184fdd9-7a6a-400d-ae35-8158d2efb090"
+#define MEG_BLE_DEVICE_INFO_CHARACTERISTIC_UUID "1f20a61f-e506-488d-bf94-393ca4cdcb28"
+#define MEG_SERIAL_BAUD_RATE 9600
+
+
+// SECTION: utility macros
+//
+// used by other parts of the codebase to do handy stuff
+#define MEG_TOKENPASTE(x, y) x##y
+#define MEG_TOKENPASTE2(x, y) MEG_TOKENPASTE(x, y)
+
+
+// SECTION: perf
+//
+// utility code for taking runtime performance measurements
+namespace meg
 {
   // a single performance counter
   struct PerfCounter final {
@@ -29,20 +42,22 @@ namespace ak
   // all globally-accessible performance counters
   struct PerfCounters final {
 
+    // returns the number of counters held within this struct
     size_t size() const
     {
       return sizeof(PerfCounters)/sizeof(PerfCounter);
     }
 
+    // returns a reference to the `i`th counter
     const PerfCounter& operator[](size_t i) const
     {
-      static_assert(sizeof(PerfCounters) % sizeof(PerfCounter) == 0, "");
-      static_assert(alignof(PerfCounters) == alignof(PerfCounter), "");
+      static_assert(sizeof(PerfCounters) % sizeof(PerfCounter) == 0, "PerfCounters should only contain PerfCounter instances");
+      static_assert(alignof(PerfCounters) == alignof(PerfCounter), "PerfCounters should only contain PerfCounter instances");
       return reinterpret_cast<const PerfCounter*>(this)[i];
     }
 
-    PerfCounter analogRead{"analogRead"};
-    PerfCounter allReads{"allReads"};
+    PerfCounter allMeasurements{"allMeasurements"};
+    PerfCounter singleMeasurement{"singleMeasurement"};
 
   } g_PerfCounters;
 
@@ -78,7 +93,7 @@ namespace ak
   // the provided counter with that measurement
   class BlockScopedPerfMeasurement final {
   public:
-    BlockScopedPerfMeasurement(PerfCounter& target) :
+    explicit BlockScopedPerfMeasurement(PerfCounter& target) :
       m_Target{target},
       m_Start{micros()}
     {
@@ -92,37 +107,144 @@ namespace ak
     PerfCounter& m_Target;
     uint64_t m_Start;
   };
+
+  // define MEG_PERF as a macro that can be enabled/disabled based on other defines
+  #ifdef MEG_DEBUG
+    #define MEG_PERF(counter) meg::BlockScopedPerfMeasurement MEG_TOKENPASTE2(timer_, __LINE__)(counter)
+  #else
+    #define MEG_PERF(counter)
+  #endif
 }
 
-#ifdef AK_DEBUG
-#define AK_PERF(counter) ak::BlockScopedPerfMeasurement AK_TOKENPASTE2(timer_, __LINE__)(counter)
-#else
-#define AK_PERF(counter)
-#endif
 
-// instrumented functions
-namespace ak
+// SECTION: 64-bit clock
+//
+// monotonically increasing 64-bit clock with no rollover
+namespace meg
 {
-  uint16_t analogRead(uint8_t pin)
+  static uint32_t g_ClockLastMicros = 0;
+  static uint64_t g_ClockAccumulator = 0;
+
+  uint64_t GetClockValue()
   {
-    AK_PERF(g_PerfCounters.analogRead);
-    return ::analogRead(pin);  // call base version
+    const uint32_t cur = micros();
+    g_ClockAccumulator += cur - g_ClockLastMicros;
+    g_ClockLastMicros = cur;
+    return g_ClockAccumulator;
   }
 }
 
+// SECTION: data measurement
+//
+// code related to actually taking+storing measurement data
+namespace meg
+{
+  struct DataMeasurement final {
+
+    DataMeasurement() = default;
+    
+    explicit DataMeasurement(int value_) :
+      time{GetClockValue()},
+      value{value_}
+    { 
+    }
+    uint64_t time = 0;  // senteniel: default ctor should mean "not measured"
+    int value = 0;
+  };
+
+  DataMeasurement ReadMeasurement(uint8_t pin)
+  {
+    MEG_PERF(g_PerfCounters.singleMeasurement);
+    return DataMeasurement{analogRead(A1)};
+  }
+}
+
+
+// SECTION: bluetooth/BLE client
+//
+// utility classes/functions for maintaining the device's BLE API
+namespace meg
+{
+  class BLEApi final {
+  public:
+    BLEApi()
+    {
+      #ifdef MEG_DEBUG
+        Serial.println("begin: initializing BLE device");
+      #endif
+
+      // init top-level device stuff + server
+      BLEDevice::init(MEG_BLE_LONG_NAME);
+      m_Server = BLEDevice::createServer();
+
+      // configurate advertising
+      //
+      // note: it needs further configuration from each service before being turned on
+      BLEAdvertising& advertising = *BLEDevice::getAdvertising();
+      {
+        advertising.setScanResponse(true);
+        advertising.setMinPreferred(0x06);  // functions that help with iPhone connections issue
+        advertising.setMinPreferred(0x12);
+      }
+
+      // init configuration service
+      //
+      // it's a top-level service that provides device information and lets the
+      // caller control the overall device
+      {
+        m_ConfigurationService = m_Server->createService(MEG_BLE_CONFIGURATION_SERVICE_UUID);
+        m_DeviceInfoCharacteristic = m_ConfigurationService->createCharacteristic(
+          MEG_BLE_DEVICE_INFO_CHARACTERISTIC_UUID,
+          NIMBLE_PROPERTY::READ
+        );
+        m_DeviceInfoCharacteristic->setValue("device info bytes here");
+        m_ConfigurationService->start();
+        advertising.addServiceUUID(MEG_BLE_CONFIGURATION_SERVICE_UUID);
+      }
+
+      // done! start advertising the server to clients
+      BLEDevice::startAdvertising();
+
+      #ifdef MEG_DEBUG
+        Serial.println("end  : initializing BLE device");
+      #endif
+    }
+
+    BLEApi(const BLEApi&) = delete;
+    BLEApi(BLEApi&&) noexcept = delete;
+    BLEApi& operator=(const BLEApi&) = delete;
+    BLEApi& operator=(BLEApi&&) noexcept = delete;
+
+    ~BLEApi()
+    {
+      BLEDevice::deinit();
+    }
+  private:
+    BLEServer* m_Server = nullptr;
+    BLEService* m_ConfigurationService = nullptr;
+    BLECharacteristic* m_DeviceInfoCharacteristic = nullptr;
+  };
+}
+
+
+// SECTION: top-level ESP32 code (main/setup/loop)
+
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(MEG_SERIAL_BAUD_RATE);
+  meg::BLEApi bleApi;
+
+  while (true)
+  {
+    // do performance-counted work
+    MEG_PERF(meg::g_PerfCounters.allMeasurements);
+    meg::DataMeasurement m1 = meg::ReadMeasurement(A1);
+    meg::DataMeasurement m2 = meg::ReadMeasurement(A1);
+    meg::DataMeasurement m3 = meg::ReadMeasurement(A1);
+  }
 }
 
 void loop()
 {
-  {
-    AK_PERF(ak::g_PerfCounters.allReads);
-    const int a1Val = ak::analogRead(A1);
-    const int a2Val = ak::analogRead(A2);
-    const int a3Val = ak::analogRead(A3);
-  }
-  
-  ak::PrintAllPerfCountersToSerial();
+  // don't use `loop`: we stack-allocate data in `setup`, rather than using globals
 }
